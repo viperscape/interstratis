ce6ba02dfde3ce4e2f134c6085b7fbd9164b7409
viperscape <chrisfgill@gmail.com>
2017-05-30 11:54:02 -0400

    wip stories view

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#[macro_use]
+extern crate rouille;
+
+use rouille::{Response};
+
+use std::fs;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    rouille::start_server("localhost:6060", move |rqs| {
+        router!(rqs,
+                (GET) (/) => {
+                    if let Ok(paths) = fs::read_dir("./stories/") {
+                        let mut stories: Vec<PathBuf> = vec![];
+                        for p in paths {
+                            if let Ok(p) = p {
+                                if p.path().is_file() {
+                                    stories.push(p.path())
+                                }
+                            }
+                        }
+                        
+                    }
+
+                    Response::html("<h3>interstratis</h3>interactive adventures")
+                },
+                _ => Response::html("<a href='/'>Nothing here</a>")
+        
+        )
+    });
 }
