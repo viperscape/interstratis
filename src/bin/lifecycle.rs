d5051f5f974d3256bc1dabcce5bf39d8d6e6d871
viperscape <chrisfgill@gmail.com>
2017-06-05 16:25:01 -0400

    fix lifecycle

--- src/bin/lifecycle.rs
+++ src/bin/lifecycle.rs
@@ -1,29 +1,15 @@
 use std::process::{Command,Child};
 use std::thread;
 use std::time::Duration;
-
-fn spawn(path: &str) -> Option<Child> {
-    let r = Command::new(path).spawn();
-    match r {
-        Ok(h) => {
-            Some(h)
-        },
-        Err(e) => {
-            println!("spawn-err:{:?}",e);
-            None
-        }
-    }
-}
+use std::env;
 
 pub fn main () {
+    let stratis_service = env::var("STRATIS_SERVICE").expect("STRATIS_SERVICE path missing");
     thread::sleep(Duration::new(1,0));
     
-    if Command::new("cargo")
-        .arg("build")
-        .arg("--bin")
-        .arg("interstratis")
-        .status().expect("failed to build").success() {
-        
-            spawn("../target/debug/interstratis.exe");
-        }
+    let r = Command::new("./inter-service.sh")
+        .current_dir(&stratis_service)
+        .status().expect("failed to build").success();
+
+    println!("Rebuilt interstratis service {:?}",r);
 }
